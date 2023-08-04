dba6c30aa3ba516a3f91a96b662fd708052e2bca
Antho <matvienko.anthony@gmail.com>
2023-08-04 17:04:28 +0200

    [Step] edit state & type

--- controllers/steps.js
+++ controllers/steps.js
@@ -15,6 +15,39 @@ exports.createStep = (req, res) => {
         .catch(error => res.status(400).json({ error }));
 }
 
+// Edit Type Step
+exports.editStepType = async (req, res) => {
+    // Empty Inputs
+    if (req.body.type === "" || req.body.type === undefined) {
+        return res.status(400).json({ message: "Merci de renseigner tous les Champs Obligatoires" });
+    }
+    const step = await models.Steps.findOne({ where: { id: req.params.id } })
+    const steps = await models.Steps.findAll({ where: { preparationId: step.preparationId } })
+    const index = steps.findIndex(s => s.type === req.body.type && s.id !== step.id)
+    if (index !== -1) {
+        return res.status(400).json({ message: "Cette étape existe déjà pour ce véhicule" });
+    } else {
+        step.update({
+            type: req.body.type
+        })
+        .then((step) => res.status(201).json(step))
+        .catch(error => res.status(400).json({ error }));
+    }
+    
+}
+
+// Edit State Step
+exports.editStateStep = (req, res) => {
+    models.Steps.findOne({ where: { id: req.params.id } })
+    .then((step) => {
+        step.update({
+            state: req.body.state
+        })
+        .then((step) => res.status(201).json(step))
+        .catch(error => res.status(400).json({ error }));
+    })
+}
+
 // Get All preparation's steps
 exports.getAllStepsByPrep = (req, res) => {
     models.Steps.findAll({ where: {preparationId: req.params.id}})
--- routes/steps.js
+++ routes/steps.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const stepCtrl = require('../controllers/steps');
 
 router.post('/', stepCtrl.createStep);
+router.put('/:id', stepCtrl.editStepType);
+router.put('/state/:id', stepCtrl.editStateStep);
 router.get('/preparation/:id', stepCtrl.getAllStepsByPrep);
 
 module.exports = router;
